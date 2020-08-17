eb985e34bc2d9c31120666d46e719a865a79e6b7
shafali03 <shaf03sm@gmail.com>
2020-08-17 11:32:51 +0100

    kata predict your age

--- Kata/Greet Me/Greet-Me.js
+++ Kata/Greet Me/Greet-Me.js
@@ -5,11 +5,6 @@
 // "riley" -- > "Hello Riley!"
 // "JACK"  -- > "Hello Jack!"
 
-// Create your own tests here. These are some of the methods available:
-//  Test.expect(boolean, [optional] message) 
-//  Test.assertEquals(actual, expected, [optional] message)
-//  Test.assertSimilar(actual, expected, [optional] message)
-//  Test.assertNotEquals(actual, expected, [optional] message) 
 
 Test.assertEquals(greet('riley'), 'Hello Riley!');
 
